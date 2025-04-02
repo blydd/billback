d64fbb84408971058e3be46b21953471eb75fc06
薄国通 <281844402@qq.com>
2025-04-02 10:26:37 +0800

    更新文档

--- src/main/java/com/example/billback/entity/Tag.java
+++ src/main/java/com/example/billback/entity/Tag.java
@@ -3,27 +3,47 @@ package com.example.billback.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.billback.enums.AccountTypeEnum;
+import com.example.billback.enums.InoutTypeEnum;
+import com.example.billback.enums.TagTypeEnum;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 @TableName("tags")
 public class Tag {
+    /**
+     * 主键ID，采用数据库自增方式
+     */
     @TableId(type = IdType.AUTO)
     private Long id;
+    
+    /**
+     * 标签名称
+     */
     private String name;
+    
     /**
      * 收支类型:1-支出,2-入账,3-不计入收支
      */
     private Integer inoutType;
+    
     /**
      * 标签类型:1-支付方式,2-账单类型
      */
     private Integer tagType;
+    
     /**
      * 账户类型.标签类型为支付方式时才有:1-储蓄账户,2-信用账户
      */
     private Integer accountType;
+    
+    /**
+     * 标签图标
+     */
     private String icon;
+    
+    /**
+     * 用户ID，关联用户表的主键
+     */
     private Long userId;
 }
