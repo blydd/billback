7d91c4c8966815bd78e4c0402ba9b2345395215b
blydd <281844402@qq.com>
2025-08-11 18:05:22 +0800

    table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,36 @@
-CREATE TABLE `bills` (
+-- 账单表
+CREATE TABLE `bill` (
                          `id` int NOT NULL AUTO_INCREMENT,
                          `user_id` int NOT NULL COMMENT '用户id',
                          `amount` decimal(10,2) NOT NULL COMMENT '金额',
                          `bill_date` timestamp NOT NULL COMMENT '账单时间',
-                         `desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '备注',
+                         `desc` text COMMENT '备注',
                          `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
                          `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-                         `type` int DEFAULT NULL COMMENT '收支类型:1-支出,2-入账',
+                         `inout_type` varchar(50) DEFAULT NULL COMMENT '收支类型:支出,入账,不计入',
                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='账单表';
-
-CREATE TABLE `tags` (
+-- 标签表
+CREATE TABLE `tag` (
                         `id` int NOT NULL AUTO_INCREMENT,
-                        `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标签名称',
-                        `inout_type` int NOT NULL COMMENT '收支类型:1-支出,2-入账,3-不计入收支',
-                        `icon` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '图标',
+                        `name` varchar(50)  NOT NULL COMMENT '标签名称',
+                        `inout_type` varchar(50) NOT NULL COMMENT '收支类型:支出,入账,不计入',
                         `user_id` int DEFAULT NULL COMMENT '用户id',
-                        `tag_type` int DEFAULT NULL COMMENT '标签类型:1-支付方式,2-账单类型',
+                        `tag_type` varchar(50) DEFAULT NULL COMMENT '标签类型:支付方式(微信支付宝等),账单类型(衣食住行等)',
                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='标签表';
-
-CREATE TABLE `bill_tags` (
+-- 账单标签关联表
+CREATE TABLE `bill_tag` (
                              `bill_id` int DEFAULT NULL,
                              `tag_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- 用户表
+CREATE TABLE `user` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `user_name` varchar(50)  NOT NULL COMMENT '用户名称',
+                          `password` varchar(50)  NOT NULL COMMENT '密码',
+                         `sex` varchar(50)  NOT NULL COMMENT '性别：男女',
+                         `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
